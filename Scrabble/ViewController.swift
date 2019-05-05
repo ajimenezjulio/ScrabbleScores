08215fdbcc61383d47c43cecba7403c7d623e5fc
Julio Aguilar Jimenez <jcaj1n17@soton.ac.uk>
2019-05-04 20:15:58 -0500

    Initial Commit

---
+++ Scrabble/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scrabble
+//
+//  Created by Julio Cesar Aguilar Jimenez on 04/05/2019.
+//  Copyright © 2019 Julio C. Aguilar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
